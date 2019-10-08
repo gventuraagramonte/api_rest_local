8b834458377a4084ef1159fea466f7b7464187f9
Gato Gordo <gventuraagramonte@gmail.com>
2019-10-08 11:51:20 -0500

    api rest local, the json is inproducts.py

---
+++ products.py
@@ -0,0 +1,11 @@
+products = [
+    {"name":"laptop",
+     "price":800,
+     "quantity":4},
+    {"name":"mouse",
+     "price":800,
+     "quantity":40},
+    {"name":"monitor",
+     "price":400,
+     "quantity":3},
+]
